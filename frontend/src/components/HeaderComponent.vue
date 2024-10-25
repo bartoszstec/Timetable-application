f39acae6b04952ec2c03eb030d62e00f041988bc
wrzoskiewicz <bartoszstec7@gmail.com>
2024-10-26 01:57:52 +0200

    Dodanie biblioteki axios i utworzenie komponentów do komunikacji z backendem, zmiana portu servera developerskiego Vue na: 3000

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -8,10 +8,11 @@
 								<header id="header">
 									<h1><router-link to="/" id="logo">minimaxing</router-link></h1>
 									<nav id="nav">
-										<a href="index.html" class="current-page-item">Homepage</a>
 										<router-link to="/">Home</router-link>
 										<router-link to="/another">Another Page</router-link>
-
+										<router-link to="/schedule">Harmonogram</router-link>
+										<router-link to="/addLesson">Add Lesson</router-link>
+										<router-link to="/showdata">show data</router-link>
 									</nav>
 								</header>
 
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,19 +1,32 @@
 // src/router.js
 import { createRouter, createWebHistory } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
 import AnotherComponent from './components/AnotherComponent.vue';
+import ScheduleComponent from './components/ScheduleComponent.vue';
+import HelloWorld from './components/HelloWorld.vue';
+import DataComponent from './components/DataComponent.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HelloWorld  // Komponent dla strony głównej
+    component:HelloWorld  // Komponent dla strony głównej
   },
   {
     path: '/another',
     name: 'another',
     component: AnotherComponent  // Komponent dla innej strony
+  },
+  {
+    path: '/schedule',
+    name: 'schedule',
+    component: ScheduleComponent  // Komponent dla strony z harmonogramem zajęć
+  },
+  {
+    path: '/showdata',
+    name: 'showdata',
+    component: DataComponent  // Komponent testowy wyświetlający dane z backendu
   }
+  
 ];
 
 // Tworzymy instancję routera
