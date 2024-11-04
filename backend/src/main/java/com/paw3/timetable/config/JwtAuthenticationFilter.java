cd111efadca9dcaa8e23ca093d8680659ac53f5b
wrzoskiewicz <bartoszstec7@gmail.com>
2024-11-04 23:08:13 +0100

    Konfiguracyjne zmiany backendu

--- backend/src/main/java/com/paw3/timetable/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/paw3/timetable/config/JwtAuthenticationFilter.java
@@ -42,6 +42,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+
+        String requestPath = request.getServletPath();
+        if (requestPath.startsWith("/auth/")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         final String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
--- backend/src/main/java/com/paw3/timetable/config/WebConfig.java
+++ backend/src/main/java/com/paw3/timetable/config/WebConfig.java
@@ -14,10 +14,10 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/*")
+                registry.addMapping("/**") // /** - umożliwienie dostępu do endpointów na każdym poziomie
                         .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("")
+                        .allowedHeaders("*") // Zezwolenie na wszystkie nagłówki
                         .allowCredentials(true);
             }
         };
